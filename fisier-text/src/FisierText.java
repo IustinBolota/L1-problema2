71a8011e3e99f7ff2e9a9424afe0a7ba798a9916
Iustin Bolota <iustin.bolota@student.tuiasi.ro>
2025-02-22 14:46:50 +0200

    Eliminare spatii multiple

--- fisier-text/src/FisierText.java
+++ fisier-text/src/FisierText.java
@@ -5,8 +5,25 @@ public class FisierText {
         FileReader fr = new FileReader("intrare.txt");
         BufferedReader br = new BufferedReader(fr);
         String line;
+        String text="";
         while((line=br.readLine())!=null) {
-            System.out.println(line);
+            //System.out.println(line);
+            text+=line;
+            text+="\n";
         }
+        text=EliminareSpatii(text);
+        System.out.print(text);
+    }
+
+    private static String EliminareSpatii(String text) {
+        for(int i=0;i<text.length();i++){
+            if(text.charAt(i)==' '){
+                int j=i+1;
+                while(text.charAt(j)==' '){
+                    text=text.substring(0,i)+text.substring(j);
+                }
+            }
+        }
+        return text;
     }
 }
